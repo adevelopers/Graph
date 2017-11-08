7cf8a724a0db3abd477d68e90c30fb5e24309a63
adeveloper <hudiakov@gmail.com>
2017-11-08 15:17:26 +0300

    Update BondNode

--- GraphGlycine/Nodes/BondNode.swift
+++ GraphGlycine/Nodes/BondNode.swift
@@ -14,8 +14,29 @@ class BondNode: SKShapeNode {
     var end: AtomNode!
     
     convenience init(_ a: AtomNode, _ b: AtomNode) {
+        self.init()
         begin = a
         end = b
+        self.fillColor = .white
+        self.strokeColor = .white
+        self.lineWidth = 6
+        self.updateBond()
+    }
+    
+    func updateBond() {
+            guard
+                let a = begin,
+                let b = end
+                else {
+                    return
+            }
+            
+            let linePath = CGMutablePath()
+            linePath.move(to: b.position)
+            linePath.addLine(to: a.position)
+            linePath.closeSubpath()
+            self.path = linePath
+
     }
     
 }
